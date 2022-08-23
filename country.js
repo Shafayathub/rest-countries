917db94e9d641e00272da61f9fb9c536e21e3d51
Shafayathub <shafayat.ms@gamilcom>
2022-08-23 17:11:00 +0600

    css added

--- country.js
+++ country.js
@@ -1,4 +1,3 @@
-document.write('Am I visible?');
 const loadCountries = () => {
   fetch('https://restcountries.com/v3.1/all')
     .then((res) => res.json())
@@ -12,14 +11,17 @@ const displayCountries = (countries) => {
   );
   //   console.log(allCountriesHTML[0]);
   const container = document.getElementById('countries');
-  container.innerHTML = allCountriesHTML.join(' ');
+  container.innerHTML = allCountriesHTML;
 };
 const getCountriesHTML = (country) => {
   return `
     <div>
-        <h2>${country.name.common}</h2>
-        <img src="${country.flags.png}"
-    </div>`;
+        <div class="country">
+          <h2>${country.name.common}</h2>
+          <img src="${country.flags.png}"
+        </div>
+    </div>
+    `;
 };
 
 loadCountries();
